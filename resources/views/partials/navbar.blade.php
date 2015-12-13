9a86909a3f7e8e1cf3d9f5786a82c9f716974755
kolya770 <grinko.kolya770@gmail.com>
2015-12-13 05:45:20 +0200

    mob v2

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,39 +1,41 @@
-<nav class="navbar navbar-bygimotic center-block">
-    <div class="container my-navbar">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-        </div>
-
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav">
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Головна<span class="caret"></span></a>
-                    <ul class="dropdown-menu my-menu">
-                        <li><a href="#">пункт меню один</a></li>
-                        <li><a href="#">пункт меню один</a></li>
-                        <li><a href="#">пункт меню один</a></li>
-                        <li><a href="#">пункт меню один</a></li>
-                        <li><a href="#">пункт меню один</a></li>
-                        <li><a href="#">пункт меню один</a></li>
-                        <li><a href="#">пункт меню один</a></li>
-                    </ul>
-                </li>
-                <li><a href="#">Ассортімент</a></li>
-
-            </ul>
-
-            <ul class="nav navbar-nav navbar-right">
-                <li><a href="#" class="orange-btn">Інформація</a></li>
-                <li><a href="#" class="position-cn">Контакті</a></li>
-            </ul>
-        </div><!-- /.navbar-collapse -->
-    </div>
+<div class="full-navbar">
     <img class="rainbow" src="img/rainbow.png"><!-- /.container-fluid -->
-</nav>
+    <nav class="navbar navbar-bygimotic center-block">
+        <div class="container my-navbar">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Головна<span class="caret"></span></a>
+                        <ul class="dropdown-menu my-menu">
+                            <li><a href="#">пункт меню один</a></li>
+                            <li><a href="#">пункт меню один</a></li>
+                            <li><a href="#">пункт меню один</a></li>
+                            <li><a href="#">пункт меню один</a></li>
+                            <li><a href="#">пункт меню один</a></li>
+                            <li><a href="#">пункт меню один</a></li>
+                            <li><a href="#">пункт меню один</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Ассортімент</a></li>
+
+                </ul>
+
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#" class="orange-btn">Інформація</a></li>
+                    <li><a href="#" class="position-cn">Контакті</a></li>
+                </ul>
+            </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+    </nav>
+</div>
